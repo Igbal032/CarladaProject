b3b78d2d605fc416b2143566f8a5e8e21016c768
Igbal032 <iqbal.hoff@list.ru>
2021-06-29 12:26:44 +0400

    CAR-112 Searching test

--- src/test/java/az/code/carlada/CarladaApplicationTests.java
+++ src/test/java/az/code/carlada/CarladaApplicationTests.java
@@ -1,13 +1,39 @@
 package az.code.carlada;
 
+import az.code.carlada.components.ModelMapperComponent;
+import az.code.carlada.daos.interfaces.SearchDAO;
+import az.code.carlada.dtos.ListingListDTO;
+import az.code.carlada.dtos.PaginationDTO;
+import az.code.carlada.services.SearchServiceImpl;
+import az.code.carlada.services.interfaces.SearchService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Map;
 
 @SpringBootTest
 class CarladaApplicationTests {
 
+    @InjectMocks
+    private SearchServiceImpl searchService;
+
+//    @MockBean
+//    private StudentRepository studentRepository;
+    @MockBean
+    SearchDAO searchDAO;
+    @MockBean
+    ModelMapperComponent mapperService;
+
     @Test
     void contextLoads() {
+        Map<String, String> params = null;
+        params.put("Color","blue");
+        PaginationDTO<ListingListDTO> result = searchService.searchListings(params);
+        System.out.println(result);
     }
 
 }
